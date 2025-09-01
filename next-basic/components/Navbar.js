faba2253437e66c85a72520884bb890bc8f52e0c
Bryan047 <117346025+Bryan047@users.noreply.github.com>
2025-09-01 15:28:17 +0700

    Start learning Next.js
    Fist learning Next.js

---
+++ next-basic/components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+export default function Navbar(){
+    return(
+            <nav>
+                <div>
+                    <Image src="/Gemini.jpg" width={50} height={50} alt="LOGO" />
+                </div>
+                <Link href="/">หน้าแรก</Link>
+                <Link href="/about">เกี่ยวกับเรา</Link>
+                <Link href="/products">สินค้าทั้งหมด</Link>
+
+            </nav>
+
+    )
+}
